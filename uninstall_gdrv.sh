8843a8218621c9ff9ffae1423a1f93a59acf6a09
cmgrass <C.grass@live.com>
2020-02-22 16:29:17 -0500

    Updated uninstall script

--- uninstall_gdrv.sh
+++ uninstall_gdrv.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
 target_module="grassdrv"
+target_device="grassdrv"
 
-echo "Uninstalling $target_module"
+echo "Uninstalling $target_module char device driver module"
 
-rmmod $target_module
+rmmod $target_module || exit 1
 
-echo "Successfully uninstalled $target_module"
+echo "Successfully uninstalled $target_module module"
+
+echo ""
+
+echo "Removing $target_device device file node"
+
+rm -f /dev/$target_device
+
+echo "Finished."
